b777e20e23e9bc73710da8054e281df238ddfe67
Fred Eisele <phreed@gmail.com>
2011-11-22 14:17:32 -0600

    commented out the python bindings for simple-wireless-tdma

--- setup.sh
+++ setup.sh
@@ -78,28 +78,27 @@ Doing so provides the following (which you should run in order):
 - ns3-dependancies : installs the ubuntu packages needed by ns-3
 - ns3-get : prepares a repos directory in the NS3_HOME 
 - ns3-configure : configure ns3 for tests and examples
-- ns3-linkup : makes links from the source files into the ns-3-dev tree
+- ns3-link : makes links from the source files into the ns-3-dev tree
 - ns3-build : build ns3 
 - ns3-install : install ns3 
 - ns3-test : build ns3 
-- ns3-unlinkup : reverses what linkup does (it removes the symbolic links)
+- ns3-unlink : reverses what linkup does (it removes the symbolic links)
 
 A sample of running these can be found in 'configure-ns3.txt' 
 EOM
 
-function ns3-linkup() {
+function ns3-link() {
 CWD=$(pwd)
+rm -f ${NS3_HOME}/src/simple-wireless-tdma 
 ln -f -s ${CWD}/ns-3-dev/src/simple-wireless-tdma ${NS3_HOME}/src/simple-wireless-tdma 
-ln -f -s ${CWD}/ns-3-dev/examples/wireless ${NS3_HOME}/examples/wireless/wifi-tdma.cc 
-ln -f -s ${CWD}/ns-3-dev/tdma.h ${NS3_HOME}/tdma.h
+
+rm -f ${NS3_HOME}/src/serial-tdma
+#ln -f -s ${CWD}/ns-3-dev/src/serial-tdma ${NS3_HOME}/src/serial-tdma
+
+rm -f ${NS3_HOME}/examples/wireless 
+ln -f -s ${CWD}/ns-3-dev/examples/wireless ${NS3_HOME}/examples/wireless 
+
+ln -f ${CWD}/ns-3-dev/tdma.h ${NS3_HOME}/tdma.h
 meld ns-3-dev/src/wifi/wscript ${NS3_HOME}/src/wifi/wscript
 }
-
-function ns3-unlinkup() {
-rm ${NS3_HOME}/src/simple-wireless-tdma 
-rm ${NS3_HOME}/examples/wireless/wifi-tdma.cc 
-rm ${NS3_HOME}/src/wifi/wscript
-rm ${NS3_HOME}/tdma.h
-}
-
 
