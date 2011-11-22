91807cf4962271a039e1a5472c1a4cfafa69d499
Fred Eisele <phreed@gmail.com>
2011-11-21 21:19:58 -0600

    still working through the tutorials

--- sims.sh
+++ sims.sh
@@ -7,25 +7,32 @@ export NS3_REPOS=/opt/ns3/repos
 fi
 export NS3_HOME="${NS3_REPOS}/ns-3-allinone/ns-3-dev"
 
-# missing texlive-pdf
-
 function sim-source() {
    source ./sims.sh
 }
 
+
+function sim-sync() {
+  EXAMPLE=./ns-3-dev/src/simple-wireless-tdma/examples/
+  rm ${NS3_HOME}/scratch/tdma-example.cc
+  rm ${NS3_HOME}/scratch/tdmaSlots.txt
+  ln ${EXAMPLE}/tdma-example.cc ${NS3_HOME}/scratch/tdma-example.cc
+  ln ${EXAMPLE}/tdmaSlots.txt ${NS3_HOME}/scratch/tdmaSlots.txt
+}
+
 function sim-strict() {
   EXAMPLE=./ns-3-dev/src/simple-wireless-tdma/examples/
-  ln -s  ${EXAMPLE}/tdma-example.cc ${NS3_HOME}/scratch/tdma-example.cc
-  ln -s  ${EXAMPLE}/tdmaSlots.txt ${NS3_HOME}/scratch/tdmaSlots.txt
   pushd "${NS3_HOME}"
-  ./waf --run=tdma-example 
+  ./waf --run "scratch/tdma-example ${1}";
   popd
 }
 
 cat <<EOM
 This script is used to run the project simulations.
 
-- sim-strict : installs the ubuntu packages needed by ns-3
+- sim-sync : sync up the source code 
+- sim-strict : runs the test application
+- sim-args : runs the test application with specified args
 
 A sample of running these can be found in 'configure-ns3.txt' 
 EOM
